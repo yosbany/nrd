445c93b5d204fe9fcf7c37baeafeab63125e193e
yosbany <ytejas.86@gmail.com>
2024-04-25 18:27:28 -0300

    ok

--- assets/js/routes.js
+++ assets/js/routes.js
@@ -4,7 +4,21 @@ export const routes = {
     'login': 'login.html',
     'not-found': 'not-found.html',
     'access-denied': 'access-denied.html',
-    'home': 'home.html'
+    'home': 'home.html',
+    'accounting-transactions': 'accounting-transactions.html',
+    'budget-lunch': 'budget-lunch.html',
+    'calculate-price': 'calculate-price.html',
+    'counter-shifts': 'counter-shifts.html',
+    'make-order': 'make-order.html',
+    'online-catalog': 'online-catalog.html',
+    'posters-A4': 'posters-A4.html',
+    'print-price': 'print-price.html',
+    'profile': 'profile.html',
+    'purchase-plan': 'purchase-plan.html',
+    'purchase-price': 'purchase-price.html',
+    'receive-order': 'receive-order.html',
+    'recipe-book': 'recipe-book.html',
+    'rrhh': 'rrhh.html'
 };
 
 // Controladores
