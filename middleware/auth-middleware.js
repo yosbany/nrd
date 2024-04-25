5d264b347e58c6b78ea85fc83492207b42af48d9
yosbany <ytejas.86@gmail.com>
2024-04-25 10:41:07 -0300

    ok

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -4,7 +4,7 @@ export function requireAuth() {
     const currentUser = getCurrentUser();
     console.log("currentUser: ",currentUser);
     if (!currentUser) {
-        window.location.href = '../login.html';
+        window.location.href = 'login.html';
     }
 }
 
@@ -12,6 +12,6 @@ export function requireRole(requiredRole) {
     const user = getCurrentUser();
     if (!user || user.role !== requiredRole) {
         // Si el usuario no tiene el rol adecuado, redirige a una página de acceso denegado
-        window.location.href = '../access-denied.html';
+        window.location.href = 'access-denied.html';
     }
 }
