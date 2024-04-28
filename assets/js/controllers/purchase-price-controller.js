3534b33ce4fec879fbf42f1396c2a5da36abed5c
yosbany <ytejas.86@gmail.com>
2024-04-28 09:55:41 -0300

    ok

--- assets/js/controllers/purchase-price-controller.js
+++ assets/js/controllers/purchase-price-controller.js
@@ -1,4 +1,4 @@
-import PurchasePriceController from './controllers/purchase-price-controller.js';
+//import PurchasePriceController from './controllers/purchase-price-controller.js';
 export default class PurchasePriceController {
     constructor() {
         console.log('PurchasePriceController constructor called');
