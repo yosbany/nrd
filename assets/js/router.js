05dd23bf239b1c1f00792e7ba5f841f661ceebc3
yosbany <ytejas.86@gmail.com>
2024-04-28 09:04:25 -0300

    ok

--- assets/js/router.js
+++ assets/js/router.js
@@ -10,18 +10,21 @@ export function handleRoute() {
 
     if (!route) {
         redirectTo('not-found.html');
+        return;
     }
-    else if (requiresAuthentication(hash)) {
+
+    if (requiresAuthentication(hash)) {
         requireAuth();
+        return;
     }
 
     if (isRedirectRoute(hash)) {
         showLoaderPage();
         redirectTo(route);
+        return;
     }
-    else {
-        loadPage(route, title, controller, hash);
-    }
+
+    loadPage(route, title, controller, hash);
 }
 
 function isRedirectRoute(hash) {
@@ -30,7 +33,7 @@ function isRedirectRoute(hash) {
 }
 
 function requiresAuthentication(hash) {
-    return hash !== '' && hash !== 'login' && hash !== 'access-denied' && hash !== 'not-found';
+    return hash !== '' && !['login', 'access-denied', 'not-found'].includes(hash);
 }
 
 function fetchAndSetHTML(url, targetElementId) {
@@ -39,97 +42,67 @@ function fetchAndSetHTML(url, targetElementId) {
         .then(html => document.getElementById(targetElementId).innerHTML = html);
 }
 
-function showLoaderPage() {
-    document.getElementById("loaderPage").classList.remove("d-none");
-    document.getElementById("loaderPage").classList.add("d-block");
-    document.getElementById("page").classList.remove("d-block");
-    document.getElementById("page").classList.add("d-none");
+function showLoader(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.classList.remove("d-none");
+        element.classList.add("d-block");
+    }
 }
 
-function hideLoaderPage() {
-    document.getElementById("loaderPage").classList.remove("d-block");
-    document.getElementById("loaderPage").classList.add("d-none");
-    document.getElementById("page").classList.remove("d-none");
-    document.getElementById("page").classList.add("d-block");
-}
-
-function showLoaderApp() {
-    document.getElementById("loaderApp").classList.remove("d-none");
-    document.getElementById("loaderApp").classList.add("d-block");
-    document.getElementById("app").classList.remove("d-block");
-    document.getElementById("app").classList.add("d-none");
-}
-
-function hideLoaderApp() {
-    document.getElementById("loaderApp").classList.remove("d-block");
-    document.getElementById("loaderApp").classList.add("d-none");
-    document.getElementById("app").classList.remove("d-none");
-    document.getElementById("app").classList.add("d-block");
+function hideLoader(elementId) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.classList.remove("d-block");
+        element.classList.add("d-none");
+    }
 }
 
 function setPageTitleAndHeader(title) {
-    document.title = "NRD - " + title;
-    document.getElementById("pageTitle").innerText = document.title;
+    const pageTitle = `NRD - ${title}`;
+    document.title = pageTitle;
+    document.getElementById("pageTitle").innerText = pageTitle;
     document.getElementById("mainTitle").innerText = title;
 }
 
 function setSidebarMenu(hash) {
-    // Busca todos los elementos <a> dentro del div con id sidebarMenu
     const sidebarLinks = document.querySelectorAll('#sidebarMenu a.nav-link');
 
-    // Itera sobre los elementos encontrados
     sidebarLinks.forEach(link => {
-        // Verifica si el href del enlace contiene '#home'
-        if (link.getAttribute('href') && link.getAttribute('href').includes('#' + hash)) {
-            // Agrega la clase 'active'
+        const href = link.getAttribute('href');
+        if (href && href.includes(`#${hash}`)) {
             link.classList.add('active');
-            // Agrega la propiedad 'aria-current="page"'
             link.setAttribute('aria-current', 'page');
         } else {
-            // Si no contiene '#home', elimina la clase 'active'
             link.classList.remove('active');
-            // Elimina la propiedad 'aria-current'
             link.removeAttribute('aria-current');
         }
     });
 }
 
 async function loadPage(route, title, controller, hash) {
-    hideLoaderPage();
-    showLoaderApp();
+    hideLoader("loaderPage");
+    showLoader("loaderApp");
 
     try {
         await fetchAndSetHTML(`./pages/${route}`, 'app');
         setPageTitleAndHeader(title);
         setSidebarMenu(hash);
-
-        console.log('Title and Sidebar updated successfully: ', title);
+        console.log('Page loaded successfully:', route);
 
         if (controller) {
             await loadController(controller);
-            console.log('Controller loaded successfully: ', controller);
         }
-
-        console.log('Page loaded successfully: ', route);
     } catch (error) {
-        console.error('Error loading page ' + route + ': ', error);
+        console.error('Error loading page:', error);
     } finally {
-        hideLoaderApp();
+        hideLoader("loaderApp");
     }
 }
 
 async function loadController(controller) {
     try {
-        let ControllerClass;
-
-        switch (controller) {
-            case 'home-controller.js':
-                ControllerClass = HomeController;
-                break;
-            default:
-                throw new Error(`Controller ${controller} not found`);
-        }
-
+        const ControllerClass = getControllerClassName(controller);
         if (ControllerClass) {
             new ControllerClass();
             console.log('Controller loaded successfully:', controller);
@@ -140,9 +113,6 @@ async function loadController(controller) {
         console.error('Error loading controller:', error);
     }
 }
-
-
-
 
 function getControllerClassName(controller) {
     return controller.replace(/\.js$/, '').split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join('');
