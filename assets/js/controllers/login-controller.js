419a62dbbb77e7d9013b7858d773b398408438b2
yosbany <ytejas.86@gmail.com>
2024-04-29 15:11:55 -0300

    ok

--- assets/js/controllers/login-controller.js
+++ assets/js/controllers/login-controller.js
@@ -1,23 +1,22 @@
 import BaseController from './base-controller.js';
 import LoginView from '../views/login-view.js';
-import FirebaseService from '../../../services/auth-service.js'; 
+import FirebaseServiceInstance from '../../../services/firebase-service.js';
 
-export default class LoginController extends BaseController{
-    
+export default class LoginController extends BaseController {
+
     constructor() {
         super();
         this.view = new LoginView();
-        this.
     }
 
     async init() {
         console.log("LoginController init");
-        document.getElementById('submitBtn').addEventListener('click', async function(event) {
+        document.getElementById('submitBtn').addEventListener('click', async function (event) {
             event.preventDefault();
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
             try {
-                await FirebaseService(email, password);
+                await FirebaseServiceInstance.login(email, password);
                 window.location.href = '#home';
             } catch (error) {
                 console.error('Error al iniciar sesión:', error.message);
