cae7da9dde51f58937c178b71c26e7f505671e5d
yosbany <ytejas.86@gmail.com>
2024-04-29 15:31:20 -0300

    ok

--- assets/js/controllers/login-controller.js
+++ assets/js/controllers/login-controller.js
@@ -17,7 +17,7 @@ export default class LoginController extends BaseController {
             const password = document.getElementById('password').value;
             try {
                 await FirebaseServiceInstance.login(email, password);
-                this.redirectToPage("/");
+                this.redirectToPage("");
             } catch (error) {
                 console.error('Error al iniciar sesión:', error.message);
             }
