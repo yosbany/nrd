6b2b7d4c99e93af537aca90db00d731a7e1caf73
yosbany <ytejas.86@gmail.com>
2024-04-29 12:02:17 -0300

    ok

--- assets/js/views/home-view.js
+++ assets/js/views/home-view.js
@@ -1,12 +1,13 @@
 import BaseView from './base-view.js';
 
 export default class HomeView extends BaseView {
+
     constructor() {
         super();
     }
     
     render() {
         console.log("HomeView render");
-        this.fetchAndSetHTML("./fragments/home.html", "app");
+        this.fetchAndSetHTML("./assets/js/views/fragments/home.html", "app");
     }
 }
