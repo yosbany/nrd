ea6ed9294106480dda2603ee3bb636c153b8a0e2
yosbany <ytejas.86@gmail.com>
2024-04-30 12:51:07 -0300

    OK

--- assets/js/models/initial-data.js
+++ assets/js/models/initial-data.js
@@ -2,39 +2,495 @@ const data = {
     "version": "1.0",
     "proveedores": [
         {
-            "proveedor": "ANTOMI",
+            "proveedor": "CARNELANDIA",
             "productos": [
                 {
-                    "producto": "producto1",
-                    "contenido": "bolsa",
-                    "precio": 23,
+                    "producto": "CARNE PICADA ESPECIAL",
+                    "contenido": "KG",
+                    "precio": 0,
+                    "stock": 6
+                },
+                {
+                    "producto": "BONDIOLA",
+                    "contenido": "PIEZA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "POLLO ENTERO",
+                    "contenido": "UN",
+                    "precio": 0,
                     "stock": 3
                 },
                 {
-                    "producto": "producto2",
-                    "contenido": "kg",
-                    "precio": 23,
-                    "stock": 4
+                    "producto": "POLLO MUSLO 1/4",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 20
+                },
+                {
+                    "producto": "MILANESA DE CARNE",
+                    "contenido": "KG",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "MILANESA DE POLLO",
+                    "contenido": "KG",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "MILANESA DE CERDO",
+                    "contenido": "KG",
+                    "precio": 0,
+                    "stock": 3
                 }
             ]
         },
         {
-            "proveedor": "BIRWOK",
+            "proveedor": "CARNICERÍA DEL SUR",
             "productos": [
                 {
-                    "producto": "producto1",
-                    "contenido": "bolsa",
-                    "precio": 23,
+                    "producto": "CARNE PICADA ESPECIAL",
+                    "contenido": "KG",
+                    "precio": 0,
+                    "stock": 6
+                },
+                {
+                    "producto": "BONDIOLA",
+                    "contenido": "PIEZA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "POLLO ENTERO",
+                    "contenido": "UN",
+                    "precio": 0,
                     "stock": 3
                 },
                 {
-                    "producto": "producto2",
-                    "contenido": "kg",
-                    "precio": 23,
-                    "stock": 4
+                    "producto": "POLLO MUSLO 1/4",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 20
+                },
+                {
+                    "producto": "MILANESA DE CARNE",
+                    "contenido": "KG",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "MILANESA DE POLLO",
+                    "contenido": "KG",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "MILANESA DE CERDO",
+                    "contenido": "KG",
+                    "precio": 0,
+                    "stock": 3
                 }
             ]
+        },
+        {
+            "proveedor": "DISTRIBUIDORA CUAREIM S.R.L. (PONTEVEDRA)",
+            "productos": [
+                {
+                    "producto": "JUGO MAXIFRUTA 1LT",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "JUGO MAXIFRUTA 20ML",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "MAYONESA PONTEVEDRA 348GR",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "MOSTAZA PONTEVEDRA 20GR",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "PURE DE TOMATE PONTEVEDRA 1030GR",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "SALSA KETCHUP PONTEVEDRA 20GR ",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "SALSA PRONTO PASTA PONTEVEDRA 340GR",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                }
+
+            ]
+        },
+        {
+            "proveedor": "FERNANDO DANIEL DELGADO PEREZ (COLONIAL)",
+            "productos": [
+                {
+                    "producto": "CREMA DE LECHE COLONIAL 250ML",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "CREMA DE LECHE COLONIAL 50ML",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "DULCE LECHE COLONIAL 250GR",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "DULCE LECHE COLONIAL 50GR",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "GELATINA COLONIAL 120GR",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 5
+                },
+                {
+                    "producto": "MUZZARELLA COLONIAL",
+                    "contenido": "BARRA",
+                    "precio": 0,
+                    "stock": 2
+                },
+                {
+                    "producto": "QUESO DE UNTAR ECOMEL 20GR",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 2
+                },
+                {
+                    "producto": "YOGURT COLONIAL 1KG",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 2
+                },
+                {
+                    "producto": "DULCE LEHCE COLONIAL BLANDO 5KG",
+                    "contenido": "TARRO",
+                    "precio": 0,
+                    "stock": 2
+                },
+                {
+                    "producto": "DULCE LEHCE COLONIAL DURO 5KG",
+                    "contenido": "TARRO",
+                    "precio": 0,
+                    "stock": 1
+                }
+            ]
+        },
+        {
+            "proveedor": "FRUTAS Y VERDURA (UAM)",
+            "productos": [
+                {
+                    "producto": "BANANA ECUADOR",
+                    "contenido": "CAJA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "TOMATE REDONDO",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "LECHUGA MANTECOSA",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 12
+                },
+                {
+                    "producto": "PAPA ROSADA",
+                    "contenido": "BOLSA",
+                    "precio": 0,
+                    "stock": 2
+                },
+                {
+                    "producto": "CEBOLLA AMARILLA",
+                    "contenido": "BOLSA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "CALABACÍN",
+                    "contenido": "BOLSA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "ZAPALLO CABUTIA",
+                    "contenido": "BOLSA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "AJO",
+                    "contenido": "BOLSITA",
+                    "precio": 0,
+                    "stock": 2
+                },
+                {
+                    "producto": "LIMÓN",
+                    "contenido": "BOLSA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "ACELGA",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 6
+                },
+                {
+                    "producto": "BONIATO CRIOLLO",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "MANDARINA CRIOLLO",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "MANZANA ROJA",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "MORRÓN ROJO",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "MORRÓN VERDE",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "PEPINO",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "PEREJIL",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "PEREJIL",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "ZANAHORIA",
+                    "contenido": "BOLSA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "ZAPALLITO",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "BERENJENA",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "CIRUELA",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "DURAZNO",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "ESPINACA",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 6
+                },
+                {
+                    "producto": "NARANJA DE MESA",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "NARANJA DE JUGO",
+                    "contenido": "CAJON",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "REMOLACHA",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 6
+                },
+                {
+                    "producto": "REPOLLO BLANCO",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 6
+                },
+                {
+                    "producto": "ZUCCHINI",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "UVA",
+                    "contenido": "PLANCHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "SANDIA",
+                    "contenido": "UN",
+                    "precio": 0,
+                    "stock": 2
+                },
+                {
+                    "producto": "PERA",
+                    "contenido": "PLACHA",
+                    "precio": 0,
+                    "stock": 1
+                },
+                {
+                    "producto": "PELON",
+                    "contenido": "PLACHA",
+                    "precio": 0,
+                    "stock": 1
+                }
+
+            ]
+        },
+        {
+            "proveedor": "GRUPO LIDERDAS",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "IRAZU DISTRIBUIDORA",
+            "productos": [
+               
+            ]
+        },
+        {
+            "proveedor": "KALTER",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "MUNDO FROZZEN",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "NACIROL",
+            "productos": [
+               
+            ]
+        },
+        {
+            "proveedor": "SAMECA",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "MONTEVIDEO REFRESCOS SRL (COCA COLA)",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "ESTABLECIMIENTOS JUAN SARUBBI S A",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "SEOR SOCIEDAD ANONIMA (DON TOTO)",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "SOSA RODRIGUEZ JORGE HECTOR (NESTLE)",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "BLANCOR S A",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "COMITAN S A",
+            "productos": [
+                
+            ]
+        },
+        {
+            "proveedor": "MALATIC (SALUS)",
+            "productos": [
+                
+            ]
         }
+
     ]
 };
 
