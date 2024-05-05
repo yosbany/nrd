c9bc9270fc7fb0d015de6fb0a1452a504ac1c642
yosbany <ytejas.86@gmail.com>
2024-05-05 18:16:02 -0300

    ok

--- assets/js/controllers/make-order-controller.js
+++ assets/js/controllers/make-order-controller.js
@@ -1,7 +1,7 @@
 import BaseController from './base-controller.js';
 import MakeOrderView from '../views/make-order-view.js';
 import LocalStorageModel from '../models/local-storage-model.js';
-import NrdApiModel from '../models/nrd-api-model.js';
+
 
 
 export default class MakeOrderController extends BaseController {
@@ -20,25 +20,9 @@ export default class MakeOrderController extends BaseController {
     async makeOrder() {
         console.log("MakeOrderController makeOrder");
         this.view.renderView();
-        this.testApi();
+        
     }
 
-    async testApi(){
-        const codes = await this.nrdApiModel.getAllCodes();
-        console.log('Todos los códigos:', codes);
-
-        const newCode = await this.nrdApiModel.addCode('nuevo_code', '{"campo": "valor"}');
-        console.log('Nuevo código añadido:', newCode);
-
-        const codeData = await this.nrdApiModel.getCodeData('nuevo_code');
-        console.log('Datos del código nuevo_code:', codeData);
-
-        const updatedCode = await this.nrdApiModel.updateCode('nuevo_code', '{"campo": "valor_actualizado"}');
-        console.log('Código actualizado:', updatedCode);
-
-        const deletedCode = await this.nrdApiModel.deleteCode('nuevo_code');
-        console.log('Código eliminado:', deletedCode);
-    }
 
 }
 
