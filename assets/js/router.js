aa4d71aad3335474c23f06c61dcdd94ce7e79d68
yosbany <ytejas.86@gmail.com>
2024-05-08 22:02:29 -0300

    ok

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,5 +1,4 @@
 import { showLoaderPage, showLoaderApp, redirectTo } from './util.js';
-import FirebaseServiceInstance from '../../services/firebase-service.js';
 import HomeController from './controllers/home-controller.js';
 import LoginController from './controllers/login-controller.js';
 import AccountingTransactionsController from './controllers/accounting-transactions-controller.js';
@@ -16,6 +15,7 @@ import PurchasePriceController from './controllers/purchase-price-controller.js'
 import ReceiveOrderController from './controllers/receive-order-controller.js';
 import RecipeBookController from './controllers/recipe-book-controller.js';
 import RrhhController from './controllers/rrhh-controller.js';
+import FirebaseService from '../../services/firebase-service.js';
 
 
 const BASE_PATH = '/nrd/';
@@ -86,7 +86,7 @@ export default async function router() {
         if (!window.location.hash) {
             executeControllerMethod(controller, 'init');
         } else {
-            const isAccess = await FirebaseServiceInstance.checkAccessCurrentUserRoutesApp(key);
+            const isAccess = await FirebaseService.checkAccessCurrentUserRoutesApp(key);
             if (isAccess) {
                 const camelCaseKey = key.includes('-') ? key.replace(/-([a-z])/g, function (match, letter) {
                     return letter.toUpperCase();
