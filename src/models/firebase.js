12382b2b633297a73b50b556b3b362f2fae94c4d
yosbany <ytejas.86@gmail.com>
2024-07-11 10:09:51 -0300

    ok

--- src/models/firebase.js
+++ src/models/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
+import { getAuth, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js';
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js';
 
 const firebaseConfig = {
@@ -7,6 +7,7 @@ const firebaseConfig = {
     authDomain: "nrd-firebase.firebaseapp.com",
     databaseURL: "https://nrd-firebase-default-rtdb.firebaseio.com",
     projectId: "nrd-firebase",
+    storageBucket: "nrd-firebase.appspot.com",
     messagingSenderId: "840023356475",
     appId: "1:840023356475:web:a7411b9b5808ac51d8581e"
 };
@@ -15,4 +16,17 @@ const firebase = initializeApp(firebaseConfig);
 const auth = getAuth(firebase);
 const db = getDatabase(firebase);
 
-export { firebase, auth, db };
+async function login(email, password) {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+        console.log('Inicio de sesión exitoso');
+    } catch (error) {
+        console.error('Error al iniciar sesión:', error.message);
+    }
+}
+
+// Ejecutar el login automáticamente
+login("nriodor@gmail.com", "NuevaR1oDor");
+
+
+export { firebase, auth, db, login };
