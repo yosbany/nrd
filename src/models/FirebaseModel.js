6a679cf07d940b824e856418c415c55e76dfaee2
yosbany <ytejas.86@gmail.com>
2024-07-11 09:55:20 -0300

    ok

--- src/models/FirebaseModel.js
+++ src/models/FirebaseModel.js
@@ -1,32 +1,25 @@
-// Importar Firebase SDK y configuración
-import { db } from './firebase.js'; // Asumiendo que './firebase' contiene la configuración de Firebase
+import { ref, set, get, push, remove } from 'https://www.gstatic.com/firebasejs/10.11.1/firebase-database.js';
+import { db } from '../firebase.js';
 
-// Objeto para manejar operaciones con Firebase
 const FirebaseModel = {
-    // Función para obtener todos los documentos de una colección
     getAll: (entity) => {
-        return db.ref(entity).once('value').then(snapshot => snapshot.val());
+        const entityRef = ref(db, entity);
+        return get(entityRef).then(snapshot => snapshot.val());
     },
 
-    // Función para obtener un documento específico por su ID
     getById: (entity, id) => {
-        return db.ref(`${entity}/${id}`).once('value').then(snapshot => snapshot.val());
+        const entityRef = ref(db, `${entity}/${id}`);
+        return get(entityRef).then(snapshot => snapshot.val());
     },
 
-    // Función para guardar o actualizar un documento
     saveOrUpdate: (entity, id, data) => {
-        if (id) {
-            // Actualizar si ya existe un ID
-            return db.ref(`${entity}/${id}`).set(data);
-        } else {
-            // Guardar un nuevo documento sin ID (Firebase generará uno nuevo)
-            return db.ref(entity).push(data);
-        }
+        const entityRef = id ? ref(db, `${entity}/${id}`) : ref(db, entity);
+        return id ? set(entityRef, data) : push(entityRef, data);
     },
 
-    // Función para eliminar un documento por su ID
     delete: (entity, id) => {
-        return db.ref(`${entity}/${id}`).remove();
+        const entityRef = ref(db, `${entity}/${id}`);
+        return remove(entityRef);
     }
 };
 
