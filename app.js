6bd22c51efd1990bf34e44562d9c5514f910071e
yosbany <ytejas.86@gmail.com>
2024-07-11 14:28:21 -0300

    ok

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ import MovementController from './src/controllers/MovementController.js';
 import RecipeController from './src/controllers/RecipeController.js';
 import TaskController from './src/controllers/TaskController.js';
 import EmployeeController from './src/controllers/EmployeeController.js';
-m.route.prefix = "/nrd";
+
+m.route.prefix = "#!/nrd";
 // Rutas de la aplicación
 m.route(document.getElementById('app'), '/', {
     '/': UserController.UserList,
