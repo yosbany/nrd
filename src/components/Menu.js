73664a3f62472ccb2e2396a69717360ea293c619
yosbany <ytejas.86@gmail.com>
2024-07-12 18:58:35 -0300

    oo

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,16 @@
-const Menu = () => {
-    return m('nav', [
-        m('h1', 'Menú Principal'),
-        m('ul', [
-            m('li', m.route.Link, { href: '/usuarios' }, 'Usuarios'),
-            m('li', m.route.Link, { href: '/proveedores' }, 'Proveedores'),
-            m('li', m.route.Link, { href: '/ordenes' }, 'Órdenes'),
-            m('li', m.route.Link, { href: '/clientes' }, 'Clientes'),
-            m('li', m.route.Link, { href: '/articulos' }, 'Artículos'),
-        ])
-    ]);
+const Menu = {
+    view: (vnode) => {
+        return m('nav', [
+            m('h1', 'Menú Principal'),
+            m('ul', [
+                m('li', m.route.Link, { href: '/usuarios' }, 'Usuarios'),
+                m('li', m.route.Link, { href: '/proveedores' }, 'Proveedores'),
+                m('li', m.route.Link, { href: '/ordenes' }, 'Órdenes'),
+                m('li', m.route.Link, { href: '/clientes' }, 'Clientes'),
+                m('li', m.route.Link, { href: '/articulos' }, 'Artículos'),
+            ])
+        ]);
+    }
 };
 
 export default Menu;
