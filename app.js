263d7856a6261181ed663bb37bab1d374b2d9181
yosbany <ytejas.86@gmail.com>
2024-07-12 18:27:21 -0300

    ok

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ import HomeController from './src/controllers/HomeController.js';
 m.route.prefix = "/nrd/#!";
 
 m.route(document.getElementById('app'), '/', {
-    '/': HomeController.home(),
-    //'/usuarios': GenericController.list('usuarios', userRenderItem),
-    //'/usuarios/nuevo': GenericController.form('usuarios', userRenderForm),
-    //'/usuarios/editar/:id': GenericController.form('usuarios', userRenderForm),
-    //'/usuarios/eliminar/:id': GenericController.delete('usuarios'),
+    '/': HomeController.home,
+    '/usuarios': GenericController.list('usuarios', userRenderItem),
+    '/usuarios/nuevo': GenericController.form('usuarios', userRenderForm),
+    '/usuarios/editar/:id': GenericController.form('usuarios', userRenderForm),
+    '/usuarios/eliminar/:id': GenericController.delete('usuarios'),
 });
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,12 +2,11 @@ const Menu = () => {
     return m('nav', [
         m('h1', 'Menú Principal'),
         m('ul', [
-            m('li', m(m.route.Link, { href: '/usuarios' }, 'Usuarios')),
-            m('li', m(m.route.Link, { href: '/proveedores' }, 'Proveedores')),
-            m('li', m(m.route.Link, { href: '/ordenes' }, 'Órdenes')),
-            m('li', m(m.route.Link, { href: '/clientes' }, 'Clientes')),
-            m('li', m(m.route.Link, { href: '/articulos' }, 'Artículos')),
-            // Agrega más enlaces según sea necesario
+            m('li', m.route.Link, { href: '/usuarios' }, 'Usuarios'),
+            m('li', m.route.Link, { href: '/proveedores' }, 'Proveedores'),
+            m('li', m.route.Link, { href: '/ordenes' }, 'Órdenes'),
+            m('li', m.route.Link, { href: '/clientes' }, 'Clientes'),
+            m('li', m.route.Link, { href: '/articulos' }, 'Artículos'),
         ])
     ]);
 };
