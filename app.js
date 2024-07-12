6570073de879bc38b7844b52c17d529ac7040963
yosbany <ytejas.86@gmail.com>
2024-07-12 19:04:03 -0300

    ok

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ import HomeController from './src/controllers/HomeController.js';
 
 m.route.prefix = "/nrd/#!";
 
-m.route(document.getElementById('app'), '/', {
-    '/': HomeController.home,
+m.route(document.getElementById('app'), '/home', {
+    '/home': HomeController.home,
     '/usuarios': GenericController.list('usuarios', userRenderItem),
     '/usuarios/nuevo': GenericController.form('usuarios', userRenderForm),
     '/usuarios/editar/:id': GenericController.form('usuarios', userRenderForm),
