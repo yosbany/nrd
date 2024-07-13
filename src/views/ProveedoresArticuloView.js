355c53fbcc9b5220ecf3d1cb3d5684f55ecc2795
yosbany <ytejas.86@gmail.com>
2024-07-13 14:57:15 -0300

    ok

--- src/views/ProveedoresArticuloView.js
+++ src/views/ProveedoresArticuloView.js
@@ -51,39 +51,47 @@ const ProveedoresArticuloView = {
             })),
 
             m('hr'),
-
-            // Select para agregar nuevos proveedores
-            m('label', 'Proveedor: '),
-            m('select', {
-                onchange: (e) => vnode.state.selectedProveedor = e.target.value
-            }, [
-                m('option', { value: '' }, 'Seleccionar Proveedor'),
-                ...todosProveedores.map(proveedor => 
-                    m('option', { value: proveedor.id }, proveedor.nombre)
-                )
+            m('div', { style: { marginBottom: '10px' } }, [
+                m('label', { style: { display: 'inline-block', width: '100px' } }, 'Proveedor:'),
+                m('select', {
+                    style: { width: '200px' },
+                    onchange: (e) => vnode.state.selectedProveedor = e.target.value
+                }, [
+                    m('option', { value: '' }, 'Seleccionar Proveedor'),
+                    ...todosProveedores.map(proveedor => 
+                        m('option', { value: proveedor.id }, proveedor.nombre)
+                    )
+                ]),
             ]),
-            m('span', ' '),
-            m('button', {
-                onclick: () => {
-                    if (selectedProveedor && !proveedores.includes(selectedProveedor)) {
-                        const articuloId = vnode.attrs.articuloId;
-                        FirebaseModel.getById('articulos', articuloId).then(articulo => {
-                            articulo.proveedores = articulo.proveedores || [];
-                            articulo.proveedores.push(selectedProveedor);
-                            FirebaseModel.saveOrUpdate('articulos', articuloId, articulo).then(() => {
-                                vnode.state.proveedores.push(selectedProveedor);
-                                vnode.state.selectedProveedor = null;
-                                m.redraw();
+            m('div', { style: { marginBottom: '10px' } }, [
+                m('label', { style: { display: 'inline-block', width: '100px' } }, 'Código Artículo:'),
+                m('input[type=text]', {
+                    value: item.nombre || '',
+                    style: { width: '200px' },
+                    onchange: (e) => item.nombre = e.target.value
+                })
+            ]),
+            m('div', { style: { marginTop: '20px' } }, [
+                m('button', {
+                    onclick: () => {
+                        if (selectedProveedor && !proveedores.includes(selectedProveedor)) {
+                            const articuloId = vnode.attrs.articuloId;
+                            FirebaseModel.getById('articulos', articuloId).then(articulo => {
+                                articulo.proveedores = articulo.proveedores || [];
+                                articulo.proveedores.push(selectedProveedor);
+                                FirebaseModel.saveOrUpdate('articulos', articuloId, articulo).then(() => {
+                                    vnode.state.proveedores.push(selectedProveedor);
+                                    vnode.state.selectedProveedor = null;
+                                    m.redraw();
+                                });
                             });
-                        });
-                    } else {
-                        alert('Este proveedor ya está agregado al artículo.');
+                        } else {
+                            alert('Este proveedor ya está agregado al artículo.');
+                        }
                     }
-                }
-            }, 'Agregar'),
-
+                }, 'Agregar')
+            ]),
             m('hr'),
-
             m('a', { href: 'javascript:void(0)', onclick: () => window.history.back() }, 'Regresar'),
             m('span', ' | '),
             m('a', { href: m.route.prefix + '/', oncreate: m.route.Link }, 'Regresar al Inicio')
