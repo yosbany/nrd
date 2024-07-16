a4bca0be4a83ad22ee71d57790c68784cf2c0de7
yosbany <ytejas.86@gmail.com>
2024-07-16 17:17:22 -0300

    ok

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,18 +5,16 @@ const Menu = {
     view: (vnode) => {
         const { isResponsive } = vnode.state;
 
-        return m('div', {
-            class: `menu ${isResponsive ? 'responsive' : ''}`
-        }, [
+        return m('nav', { class: `menu ${isResponsive ? 'responsive' : ''}` }, [
             m('a', {
                 href: 'javascript:void(0);',
                 class: 'icon',
                 onclick: () => vnode.state.isResponsive = !isResponsive
             }, m('i', { class: 'fa fa-bars' })),
-            m(m.route.Link, { href: '/usuarios' }, 'Usuarios'),
-            m(m.route.Link, { href: '/proveedores' }, 'Proveedores'),
-            m(m.route.Link, { href: '/ordenes' }, 'Órdenes'),
-            m(m.route.Link, { href: '/articulos' }, 'Artículos')
+            m(m.route.Link, { href: '/usuarios', class: 'menu-item' }, 'Usuarios'),
+            m(m.route.Link, { href: '/proveedores', class: 'menu-item' }, 'Proveedores'),
+            m(m.route.Link, { href: '/ordenes', class: 'menu-item' }, 'Órdenes'),
+            m(m.route.Link, { href: '/articulos', class: 'menu-item' }, 'Artículos')
         ]);
     }
 };
