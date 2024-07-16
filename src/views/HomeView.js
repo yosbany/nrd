4e48649fe4125c0bc86b9ad48c9e24c5558db391
yosbany <ytejas.86@gmail.com>
2024-07-16 11:16:14 -0300

    ok

--- src/views/HomeView.js
+++ src/views/HomeView.js
@@ -3,8 +3,7 @@ import Menu from '../components/Menu.js';
 const HomeView = {
     view: () => {
         return m('div', [
-            m('h1', 'Bienvenido a la Aplicación'),
-            m(Menu),
+            m('h1', 'Bienvenido a la Aplicación')
         ]);
     }
 };
