75987629c2884122d3da138dcf60b0ce26be58fe
yosbany <ytejas.86@gmail.com>
2024-07-16 16:57:45 -0300

    ok

--- src/views/UsuarioFormView.js
+++ src/views/UsuarioFormView.js
@@ -2,7 +2,7 @@ import FirebaseModel from '../models/FirebaseModel.js';
 import Link from '../components/base/Link.js';
 import Button from '../components/base/Button.js';
 import InputText from '../components/base/InputText.js';
-
+import VerticalLayout from '../components/base/VerticalLayout.js';
 
 const UsuarioFormView = {
     oninit: (vnode) => {
@@ -18,25 +18,25 @@ const UsuarioFormView = {
     view: (vnode) => {
         const item = vnode.state.item;
 
-        return m('form', {
-            onsubmit: (e) => {
-                e.preventDefault();
-                FirebaseModel.saveOrUpdate('usuarios', vnode.attrs.id, item).then(() => {
-                    m.route.set('/usuarios');
-                });
-            }
-        }, [
-            m('div.form-group', [
-                m('label', { class: 'form-label' }, 'Nombre:'),
+        return m(VerticalLayout, [
+            m('h2', vnode.attrs.id ? 'Editar Usuario' : 'Crear Usuario'),
+            m('form', {
+                onsubmit: (e) => {
+                    e.preventDefault();
+                    FirebaseModel.saveOrUpdate('usuarios', vnode.attrs.id, item).then(() => {
+                        m.route.set('/usuarios');
+                    });
+                }
+            }, [
                 m(InputText, {
+                    label: 'Nombre',
                     value: item.nombre || '',
                     oninput: (e) => item.nombre = e.target.value
-                })
-            ]),
-            m('div.form-actions', [
-                m(Button, { type: 'submit', label: 'Guardar' }),
-                m('span', ' '),
-                m(Link, { href: 'javascript:void(0)', onclick: () => window.history.back(), text: 'Cancelar' })
+                }),
+                m('div.form-actions', [
+                    m(Button, { type: 'submit', label: 'Guardar' }),
+                    m(Link, { href: 'javascript:void(0)', onclick: () => window.history.back(), text: 'Cancelar' })
+                ])
             ])
         ]);
     }
