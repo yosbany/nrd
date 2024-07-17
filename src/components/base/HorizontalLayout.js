5c07d0a8c2f70712f42fe7b1a4380fdb4f5f856e
yosbany <ytejas.86@gmail.com>
2024-07-17 15:03:30 -0300

    ok

--- src/components/base/HorizontalLayout.js
+++ src/components/base/HorizontalLayout.js
@@ -1,11 +1,19 @@
 const HorizontalLayout = {
     view: ({ attrs, children }) => {
         const { columns = [], className = '', ...rest } = attrs;
-        return m('div', { class: `row ${className}`, ...rest }, 
-            children.map((child, index) => 
+
+        // Verifica que children no esté vacío o no sea undefined
+        if (!children) {
+            return m('div', { class: `row ${className}`, ...rest });
+        }
+
+        return m('div', { class: `row ${className}`, ...rest },
+            // Mapea sobre los children y aplica las clases de columna según la configuración
+            children.map((child, index) =>
                 m('div', { class: `${columns[index] || 'twelve'} columns` }, child)
             )
         );
     }
 };
+
 export default HorizontalLayout;
