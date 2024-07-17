8b2f60fbe92634cfac844d9fc7d3162539d30bf2
yosbany <ytejas.86@gmail.com>
2024-07-17 09:01:23 -0300

    ok

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,10 @@ import Menu from './Menu.js';
 import VerticalLayout from './base/VerticalLayout.js';
 
 const App = {
-    view: () => {
+    view: ({ children }) => {
         return m(VerticalLayout, [
             m(Menu),
-            m('div', { class: 'container' }, [
-
-            ])  
+            m('div', { class: 'container' }, children)
         ]);
     }
 };
